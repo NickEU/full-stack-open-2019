6b7d98357dd10cb8e63b8ef466e5c9afbffb789d
Nick <neverate@gmail.com>
2019-07-29 19:31:55 +0300

    added notes from part3

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -1,10 +1,80 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World");
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true
+  }
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello there, World!</h1>");
 });
 
-const port = 3003;
-app.listen(port);
-console.log(`Server is running on port ${port}`);
+app.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find(note => note.id === id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter(note => note.id !== id);
+  res.status(204).end();
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(note => note.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/notes", (req, res) => {
+  const body = req.body;
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: "content missing"
+    });
+  }
+
+  const note = {
+    id: generateId(),
+    content: body.content,
+    date: new Date(),
+    important: body.important || false
+  };
+
+  notes = notes.concat(note);
+
+  res.json(note);
+});
+
+const PORT = 3003;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
