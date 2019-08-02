c18413fc248d13f1453bcb89a18fe5067c367058
Nick <neverate@gmail.com>
2019-08-02 19:41:00 +0300

    finished part 3

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -169,7 +169,6 @@ app.use(unknownEndpoint);
 
 const errorHandler = (error, req, res, next) => {
   console.error(error.message);
-
   if (error.name === "CastError" && error.kind == "ObjectId") {
     return res.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
